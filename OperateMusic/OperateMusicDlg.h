f86825e5fad8351046a0501d51c9fe97d148d6a4
AlisD <tanxiaolin1998@163.com>
2020-07-07 15:53:28 +0800

    First commit  2020/0707 1.0v

---
+++ OperateMusic/OperateMusicDlg.h
@@ -0,0 +1,34 @@
+﻿
+// OperateMusicDlg.h: 头文件
+//
+
+#pragma once
+
+
+// COperateMusicDlg 对话框
+class COperateMusicDlg : public CDialogEx
+{
+// 构造
+public:
+	COperateMusicDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_OPERATEMUSIC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
